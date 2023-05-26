f0876a5aa4b08ffb41e06c21abc9fbec5597fd8e
Ali Ghosn <alighosn2099@gmail.com>
2023-05-26 00:50:01 -0400

    Color change

--- src/pages/all-properties.tsx
+++ src/pages/all-properties.tsx
@@ -1,9 +1,11 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
 import { useMemo } from "react";
 import { Add } from "@mui/icons-material";
 import { useTable } from "@pankod/refine-core";
 import { Box, Typography, Stack, TextField, Select, MenuItem, Menu } from "@pankod/refine-mui";
 import { useNavigate } from "@pankod/refine-react-router-v6";
-import { createTheme } from "@pankod/refine-mui";
 
 import { PropertyCard, CustomButton } from "components";
 
@@ -33,100 +35,109 @@ const AllProperties = () => {
   if(isLoading) return <Typography>Is loading...</Typography>
   if(isError) return <Typography>Error...</Typography>
 
+  const lightTheme = createTheme({
+    palette: {
+       mode: "light"
+    },
+ });
+
   return (
-    <Box>
-      <Box mt='20px' sx={{
-        display: 'flex',
-        flexWrap: 'wrap',
-        gap: 3
-      }}>
-        <Stack direction='column' width='100%'>
-          <Typography fontSize={25} fontWeight={700} color='#11142d'>
-            {allProperties.length ? 'All Properties' : 'There are no properties'}
-          </Typography>
-          <Box mb={2} mt={3} display='flex' width='84%' justifyContent='space-between' flexWrap='wrap'>
-            <Box display='flex' gap={2} flexWrap='wrap' mb={{xs: '20px', sm: 0}}> 
-              <CustomButton 
-                title={`Sort Price ${ currentPrice === 'asc' ? '↑' : '↓'}`}
-                color='#fcfcfc'
-                backgroundColor="#475be8"
-                handleClick={ () => toggleSort('price') }
-              />
-              <TextField variant="outlined" color='info' placeholder="Search by title" value={ currentFilterValues.title } 
-                onChange={(e) => {
-                  setFilters([
-                    {
-                      field: 'title',
-                      operator: 'contains',
-                      value: e.currentTarget.value ? e.currentTarget.value : undefined
-                    }
-                  ])
-                }}
-              />
-              <Select variant="outlined" color="info" displayEmpty required inputProps={{'aria-label': 'Without label'}} defaultValue='' value={ currentFilterValues.propertyType } 
-                onChange={(e) => {
-                  setFilters([
-                    {
-                      field: 'propertyType',
-                      operator: 'eq',
-                      value: e.target.value
-                    }
-                  ], 'replace')
-                }}>
-                <MenuItem value=''>All</MenuItem>
-                {
-                  ['Apartment', 'Villa', 'Farmhouse', 'Condos', 'Townhouse', 'Duplex', 'Studio', 'Chalet'].map((type) => (
-                    <MenuItem key={type} value={ type.toLowerCase() }>{ type }</MenuItem>
-                  ))
-                }
-              </Select>
+    <ThemeProvider theme={lightTheme}>
+      <CssBaseline />
+      <Box>
+        <Box mt='20px' sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          gap: 3
+        }}>
+          <Stack direction='column' width='100%'>
+            <Typography fontSize={25} fontWeight={700} color='#11142d'>
+              {allProperties.length ? 'All Properties' : 'There are no properties'}
+            </Typography>
+            <Box mb={2} mt={3} display='flex' width='84%' justifyContent='space-between' flexWrap='wrap'>
+              <Box display='flex' gap={2} flexWrap='wrap' mb={{xs: '20px', sm: 0}}> 
+                <CustomButton 
+                  title={`Sort Price ${ currentPrice === 'asc' ? '↑' : '↓'}`}
+                  color='#fcfcfc'
+                  backgroundColor="#475be8"
+                  handleClick={ () => toggleSort('price') }
+                />
+                <TextField variant="outlined" color='info' placeholder="Search by title" value={ currentFilterValues.title } 
+                  onChange={(e) => {
+                    setFilters([
+                      {
+                        field: 'title',
+                        operator: 'contains',
+                        value: e.currentTarget.value ? e.currentTarget.value : undefined
+                      }
+                    ])
+                  }}
+                />
+                <Select variant="outlined" color="info" displayEmpty required inputProps={{'aria-label': 'Without label'}} defaultValue='' value={ currentFilterValues.propertyType } 
+                  onChange={(e) => {
+                    setFilters([
+                      {
+                        field: 'propertyType',
+                        operator: 'eq',
+                        value: e.target.value
+                      }
+                    ], 'replace')
+                  }}>
+                  <MenuItem value=''>All</MenuItem>
+                  {
+                    ['Apartment', 'Villa', 'Farmhouse', 'Condos', 'Townhouse', 'Duplex', 'Studio', 'Chalet'].map((type) => (
+                      <MenuItem key={type} value={ type.toLowerCase() }>{ type }</MenuItem>
+                    ))
+                  }
+                </Select>
+              </Box>
             </Box>
-          </Box>
+          </Stack>
+
+        </Box>
+
+        <Stack direction='row' justifyContent='space-between' alignItems='center'>
+          <CustomButton title="Add Property" handleClick={() => navigate('/properties/create')} backgroundColor='#475be8' color='#fcfcfc' icon={<Add />} />
         </Stack>
+        <Box mt='20px' sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          gap: 3
+        }}>
+          {
+            allProperties.map((property) => (
+              <PropertyCard 
+                key={property._id}
+                id={property._id}
+                title={property.title}
+                price={property.price}
+                location={property.location}
+                photo={property.photo}
+              /> 
+            ))
+          }
+        </Box>
 
-      </Box>
-
-      <Stack direction='row' justifyContent='space-between' alignItems='center'>
-        <CustomButton title="Add Property" handleClick={() => navigate('/properties/create')} backgroundColor='#475be8' color='#fcfcfc' icon={<Add />} />
-      </Stack>
-      <Box mt='20px' sx={{
-        display: 'flex',
-        flexWrap: 'wrap',
-        gap: 3
-      }}>
-        {
-          allProperties.map((property) => (
-            <PropertyCard 
-              key={property._id}
-              id={property._id}
-              title={property.title}
-              price={property.price}
-              location={property.location}
-              photo={property.photo}
-            /> 
-          ))
+        {allProperties.length > 0 &&
+        <Box display='flex' gap={2} mt={3} flexWrap='wrap' color='#11142d'>
+          <CustomButton title="Previous" handleClick={() => setCurrent(prev => prev-1)} backgroundColor="#475be8" color="#fcfcfc" disabled={!(current > 1)} />
+          <Box 
+            display={{xs: 'hidden', sm: 'flex'}}
+            alignItems='center'
+            gap='5px'
+          >
+            Page{` `}<strong>{ current } of { pageCount }</strong>
+          </Box>
+          <CustomButton title="Next" handleClick={() => setCurrent(prev => prev+1)} backgroundColor="#475be8" color="#fcfcfc" disabled={(current === pageCount)} />
+          <Select variant="outlined" color="info" displayEmpty required inputProps={{'aria-label': 'Without label'}} defaultValue={10} onChange={(e) => setPageSize(e.target.value ? Number(e.target.value) : 10)}>
+            {
+              [10, 20, 30, 40, 50].map((size) => <MenuItem key={size} value={size}>Show { size }</MenuItem>)
+            }
+          </Select>
+        </Box>
         }
       </Box>
-
-      {allProperties.length > 0 &&
-      <Box display='flex' gap={2} mt={3} flexWrap='wrap' color='#11142d'>
-        <CustomButton title="Previous" handleClick={() => setCurrent(prev => prev-1)} backgroundColor="#475be8" color="#fcfcfc" disabled={!(current > 1)} />
-        <Box 
-          display={{xs: 'hidden', sm: 'flex'}}
-          alignItems='center'
-          gap='5px'
-        >
-          Page{` `}<strong>{ current } of { pageCount }</strong>
-        </Box>
-        <CustomButton title="Next" handleClick={() => setCurrent(prev => prev+1)} backgroundColor="#475be8" color="#fcfcfc" disabled={(current === pageCount)} />
-        <Select variant="outlined" color="info" displayEmpty required inputProps={{'aria-label': 'Without label'}} defaultValue={10} onChange={(e) => setPageSize(e.target.value ? Number(e.target.value) : 10)}>
-          {
-            [10, 20, 30, 40, 50].map((size) => <MenuItem key={size} value={size}>Show { size }</MenuItem>)
-          }
-        </Select>
-      </Box>
-      }
-    </Box>
+    </ThemeProvider>
   )
 }
 
